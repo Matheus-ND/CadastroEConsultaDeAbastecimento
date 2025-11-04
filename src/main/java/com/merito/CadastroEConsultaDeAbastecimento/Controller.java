eec5c0ce88cc3aa4d3a55c95d1cb12a818ead0c5
Matheus-ND <daniels109@proton.me>
2025-11-04 09:44:03 -0300

    Criado a controller

---
+++ src/main/java/com/merito/CadastroEConsultaDeAbastecimento/Controller.java
@@ -0,0 +1,20 @@
+package com.merito.CadastroEConsultaDeAbastecimento;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return " Mensagem ";
+    }
+
+
+
+
+
+}
