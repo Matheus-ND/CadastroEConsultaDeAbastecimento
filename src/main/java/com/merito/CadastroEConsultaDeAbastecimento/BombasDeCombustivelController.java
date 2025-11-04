6b6384bd0a7939bc43dde22a0c6a8254140affc5
Matheus-ND <daniels109@proton.me>
2025-11-04 11:22:49 -0300

    Cria uma entidade no banco de dados apartir do BombasDeCombustivelModel

--- src/main/java/com/merito/CadastroEConsultaDeAbastecimento/BombasDeCombustivelController.java
+++ src/main/java/com/merito/CadastroEConsultaDeAbastecimento/BombasDeCombustivelController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class BombasDeCombustivelController {
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
--- src/main/java/com/merito/CadastroEConsultaDeAbastecimento/BombasDeCombustivelModel.java
+++ src/main/java/com/merito/CadastroEConsultaDeAbastecimento/BombasDeCombustivelModel.java
@@ -1,4 +1,38 @@
 package com.merito.CadastroEConsultaDeAbastecimento;
 
+
+import jakarta.persistence.*;
+
+// Entity transforma uma classe em uma entidade do BD
+@Entity
+@Table(name = "tb_cadastro_de_bombas_de_combustivel")
 public class BombasDeCombustivelModel {
+
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nomeDaBomba;
+    String CombustivelQueAbastece;
+
+
+    public BombasDeCombustivelModel(String nomeDaBomba, String combustivelQueAbastece) {
+        this.nomeDaBomba = nomeDaBomba;
+        this.CombustivelQueAbastece = combustivelQueAbastece;
+    }
+
+    public String getNomeDaBomba() {
+        return nomeDaBomba;
+    }
+
+    public void setNomeDaBomba(String nomeDaBomba) {
+        this.nomeDaBomba = nomeDaBomba;
+    }
+
+    public String getCombustivelQueAbastece() {
+        return CombustivelQueAbastece;
+    }
+
+    public void setCombustivelQueAbastece(String combustivelQueAbastece) {
+        CombustivelQueAbastece = combustivelQueAbastece;
+    }
 }
