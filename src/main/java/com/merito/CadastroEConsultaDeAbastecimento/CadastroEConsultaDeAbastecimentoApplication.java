1d1df423518c67373ac03a5ef88f67acceccce51
Matheus-ND <daniels109@proton.me>
2025-11-04 08:58:59 -0300

    Initial commit

---
+++ src/main/java/com/merito/CadastroEConsultaDeAbastecimento/CadastroEConsultaDeAbastecimentoApplication.java
@@ -0,0 +1,13 @@
+package com.merito.CadastroEConsultaDeAbastecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEConsultaDeAbastecimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEConsultaDeAbastecimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merito/CadastroEConsultaDeAbastecimento/CadastroEConsultaDeAbastecimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merito.CadastroEConsultaDeAbastecimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEConsultaDeAbastecimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
