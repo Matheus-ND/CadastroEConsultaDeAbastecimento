58a7108c02ce3ea91d05be7abfb6be4563fefdb9
Matheus-ND <daniels109@proton.me>
2025-11-04 19:59:41 -0300

    d

--- src/main/java/com/merito/CadastroEConsultaDeAbastecimento/CadastroEConsultaDeAbastecimentoApplication.java
+++ src/main/java/com/merito/CadastroEConsultaDeAbastecimento/CadastroEConsultaDeAbastecimentoApplication.java
@@ -1,4 +1,4 @@
-package com.merito.CadastroEConsultaDeAbastecimento.BombasDeCombustivel;
+package com.merito.CadastroEConsultaDeAbastecimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
