5afa56c78cfaea88a15c55c452e30701a9b5918a
Matheus-ND <daniels109@proton.me>
2025-11-04 22:00:46 -0300

    build: Conecta o banco de dados H2 a minha aplicacao SPRING

--- src/main/java/com/merito/CadastroEConsultaDeAbastecimento/TiposDeCombustivel/TiposDeCombustivelModel.java
+++ src/main/java/com/merito/CadastroEConsultaDeAbastecimento/TiposDeCombustivel/TiposDeCombustivelModel.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
-@Table(name = " tb_tiposdecombustvel")
+@Table(name = " tb_tiposdecombustivel")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
