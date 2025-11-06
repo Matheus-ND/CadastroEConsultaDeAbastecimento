d2a1d6b9e6caedd721289803db8da76d7c6b46cb
Matheus-ND <daniels109@proton.me>
2025-11-06 16:12:21 -0300

    Chore: Inclui a versao do DTO pela entidade e mapeia manualmente no BombasDeCombustivelMapper os caminhos entidade x dto

--- src/main/java/com/merito/CadastroEConsultaDeAbastecimento/BombasDeCombustivel/BombasDeCombustivelDTO.java
+++ src/main/java/com/merito/CadastroEConsultaDeAbastecimento/BombasDeCombustivel/BombasDeCombustivelDTO.java
@@ -1,4 +1,19 @@
 package com.merito.CadastroEConsultaDeAbastecimento.BombasDeCombustivel;
+import com.merito.CadastroEConsultaDeAbastecimento.TiposDeCombustivel.TiposDeCombustivelModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BombasDeCombustivelDTO {
+
+    private Long id;
+    private String nomeDaBomba;
+    private String imgUrl;
+    private List<TiposDeCombustivelModel> tiposDeCombustivel;
+
 }
