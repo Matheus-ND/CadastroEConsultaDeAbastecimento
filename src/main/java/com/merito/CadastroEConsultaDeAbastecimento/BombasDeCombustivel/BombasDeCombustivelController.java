6a724851495ff08bddef73dccb412855d008e227
Matheus-ND <daniels109@proton.me>
2025-11-07 10:44:10 -0300

    refactor: refatora o controller para incluir o responseEntity nas minhas rotas e customizar a resposta das requisicoes do cliente

--- src/main/java/com/merito/CadastroEConsultaDeAbastecimento/BombasDeCombustivel/BombasDeCombustivelController.java
+++ src/main/java/com/merito/CadastroEConsultaDeAbastecimento/BombasDeCombustivel/BombasDeCombustivelController.java
@@ -1,4 +1,7 @@
 package com.merito.CadastroEConsultaDeAbastecimento.BombasDeCombustivel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -20,33 +23,58 @@ public class BombasDeCombustivelController {
     //CRRUD
     //adicionar bomba (CREATE)
     @PostMapping("/criar")
-    public BombasDeCombustivelDTO criarBomba(@RequestBody BombasDeCombustivelDTO bomba){
-        return bombasDeCombustivelService.criarBomba(bomba);
+    public ResponseEntity <String> criarBomba(@RequestBody BombasDeCombustivelDTO bomba) {
+        BombasDeCombustivelDTO novaBomba = bombasDeCombustivelService.criarBomba(bomba);
+       return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Boomba criada com sucesso: " + novaBomba.getNomeDaBomba() + " (ID): " + novaBomba.getId());
+
     }
 
     //mostrar todas as bombas (READ)
     @GetMapping("/listar")
-    public List<BombasDeCombustivelDTO> listarBombas() {
-        return bombasDeCombustivelService.listarBombas();
+    public ResponseEntity<List<BombasDeCombustivelDTO>> listarBombas() {
+        List<BombasDeCombustivelDTO> bombas = bombasDeCombustivelService.listarBombas();
+        return ResponseEntity.ok(bombas);
     }
 
     //mostrar bombas por id (READ)
     @GetMapping("/listar/{id}")
-    public BombasDeCombustivelDTO listarBombasPorId(@PathVariable Long id) {
-        return bombasDeCombustivelService.listarBombasPorId(id);
+    public ResponseEntity<?> listarBombasPorId(@PathVariable Long id) {
+        BombasDeCombustivelDTO bombas = bombasDeCombustivelService.listarBombasPorId(id);
+        if(bombas !=null) {
+            return ResponseEntity.ok(bombas);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(" Bomba com id: " + id + " nao existe nos nossos registros");
+        }
     }
 
     //alterar dados das bombas (UPDATE)
         @PutMapping("/alterar/{id}")
-        public BombasDeCombustivelDTO alterarBombaPorId(@PathVariable Long id, @RequestBody BombasDeCombustivelDTO bombaAtualizada) {
-            return bombasDeCombustivelService.atualizarBomba(id, bombaAtualizada);
+        public ResponseEntity<?> alterarBombaPorId(@PathVariable Long id, @RequestBody BombasDeCombustivelDTO bombaAtualizada) {
+           BombasDeCombustivelDTO bombas =  bombasDeCombustivelService.atualizarBomba(id, bombaAtualizada);
+           if (bombas != null) {
+               return  ResponseEntity.ok(bombas);
+           } else {
+               return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                       .body(" Bomba com id: " + id + " nao existe nos nossos registros");
+           }
         }
 
 
     //deletar bomba (DELETE)
     @DeleteMapping("/deletar/{id}")
-    public void deletarBombaPorId(@PathVariable Long id) {
-        bombasDeCombustivelService.deletarBombaPorId(id);
+    public ResponseEntity<String> deletarBombaPorId(@PathVariable Long id) {
+
+        if ( bombasDeCombustivelService.listarBombasPorId(id) != null) {
+            bombasDeCombustivelService.deletarBombaPorId(id);
+            return ResponseEntity.ok("Bomba com o ID " + id + " deletado com sucesso");
+
+        }else {
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(" Bomba com o id " + id + " nao encontrado");
+        }
+
     }
 
 
